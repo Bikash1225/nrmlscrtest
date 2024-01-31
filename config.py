1a688f910589fa912ea57717cad51a2067d75a04
٭Bιĸαѕн٭ <127423650+Bikash1225@users.noreply.github.com>
2024-01-31 23:04:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from os import getenv
 from dotenv import load_dotenv
-load_dotenv()
 
-API_ID = int(getenv("API_ID",""))
-API_HASH = getenv("API_HASH","")
-SESSION = getenv("SESSION", "")
-SEND_ID = int(getenv("SEND_ID",""))
+load_dotenv()
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+SESSION = getenv("SESSION")
+SEND_ID = int(getenv("SEND_ID")
