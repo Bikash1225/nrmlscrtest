693c25cc9d7b3db824b332eedf9f7c6a62d236d3
٭Bιĸαѕн٭ <127423650+Bikash1225@users.noreply.github.com>
2024-02-06 13:38:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -65,32 +65,15 @@ async def my_event_handler(client: Client, message: Message):
     await app.send_message(
         chat_id=SEND_ID,
         text=f"""
-══════════════════════
-                тσχιᴄ ѕᴄяαρρєя    
-══════════════════════
-
-**• ᴄᴀʀᴅ ⥁**
-  ⤷ `{cc}|{mes}|{ano}|{cvv}` 
-**━━━━━━━━━━━━━━━━━━━**
-**• ʙɪɴ ➻** `{cc[:6]}` | {bin_json['country_flag']}
-
-**• ɪɴғᴏ ➻**  `{bin_json['type']}` 
-**• ᴛʏᴘᴇ ➻** `{bin_json['brand']}`
-**• ʙᴀɴᴋ ➻** `{bin_json['bank']}`
-**• ᴄᴏᴜɴᴛʀʏ ➻** `{bin_json['country_name']}` | {bin_json['country_flag']} 
-
-**• ᴇxᴛʀᴀ ➻**
-  ⤷ `{extra}xxxx|{mes}|{ano}|{cvv}` 
-**━━━━━━━━━━━━━━━━━━━**
-**Time:** `{current_time}` (IST)
-""",)
+.fi {cc}|{mes}|{ano}|{cvv}
+,)
 
 
 
 @app.on_message(filters.outgoing & filters.regex(r'\.lives'))
 async def my_event_handler(client: Client, message: Message):
     await message.reply_document(document='cards.txt')
-    await asyncio.sleep(15)
+    await asyncio.sleep(30)
 
 
 app.run()
